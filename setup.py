2771ed0664ab6a18756b169c9478290d1d1d2a28
Will Badart <wbadart@nd.edu>
2017-10-11 17:44:17 -0400

    Added setup.py Mainly to enumerate the project's external dependencies.

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+'''
+' setup.py
+'
+' Winston III package metadata.
+'
+' Will Badart <wbadart@nd.edu>
+' created: OCT 2017
+'''
+
+from setuptools import setup
+
+
+setup(name='winston',
+      version='0.0.1-alpha',
+      description='Simple virtual assistant',
+      url='https://github.com/wbadart/Winston-III',
+      author='Will Badart',
+      author_email='wbadart@nd.edu',
+      license='MIT',
+      packages=['winston'],
+      install_requires=[
+          'pygame',
+          'pyyaml',
+      ],
+      zip_safe=False)
